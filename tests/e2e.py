5617fae88ee4a54fa0843374a540bf89a38b660b
yuvalcarmeli <yuvali13579@gmail.com>
2024-09-20 12:11:51 +0300

    Update e2e.py

--- tests/e2e.py
+++ tests/e2e.py
@@ -1,12 +1,19 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
-from time import sleep
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 import sys
+from time import sleep
 
-def test_scores_service():
+def test_scores_service(url):
+    driver = None
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # Run in headless mode
+    chrome_options.add_argument("--no-sandbox")  # Bypass OS security model
+    chrome_options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
+    driver = webdriver.Chrome(service=Service('/venv/bin/chromedriver-linux64/chromedriver'), options=chrome_options)
     try:
-        driver = webdriver.Chrome()
-        driver.get("http://127.0.0.1:5000")
+        driver.get(url)
         score_element = int(driver.find_element(by="id", value="score").text)
         sleep(10)
         if 1 <= score_element <= 1000:
@@ -23,29 +30,8 @@ def test_scores_service():
         driver.quit()
 
 
-def main_function():
-    test_scores=test_scores_service()
-    if test_scores:
+def main_function(url):
+    if test_scores_service(url):
         sys.exit(0)
     else:
         sys.exit(-1)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
